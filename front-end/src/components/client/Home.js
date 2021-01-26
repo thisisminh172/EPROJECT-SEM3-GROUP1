76413ec95a586ea1d24fe8587cfa96e5ed0da7e2
thisisminh172 <thisisminh172@gmail.com>
2021-01-26 11:10:07 +0700

    add front-end

--- front-end/src/components/client/Home.js
+++ front-end/src/components/client/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>
+        <div className="section">
             <h1> this is home</h1>
         </div>
     )
---
+++ front-end/src/components/client/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Product() {
+    return (
+        <div>
+            this is detail product
+        </div>
+    )
+}
+
+export default Product
