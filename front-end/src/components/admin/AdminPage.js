b2e358fe3b8bc90436d9d643b64e0aa979d0a9ab
cuongoclag <57032813+cuongoclag@users.noreply.github.com>
2021-01-26 11:46:10 +0700

    next

--- front-end/src/components/admin/AdminPage.js
+++ front-end/src/components/admin/AdminPage.js
@@ -2,17 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import {useState} from 'react'
 function AdminHome() {
-
-    const [sidebarOpen, setSibarOpen] = useState(false);
     
-    const openSidebar = () => {
-        setSibarOpen(true);
-    }
-
-    const closeSidebar = () => {
-        setSibarOpen(false);
-    }
-
     return (
         <div className="container">
             <h1>Dashboard Admin</h1>
