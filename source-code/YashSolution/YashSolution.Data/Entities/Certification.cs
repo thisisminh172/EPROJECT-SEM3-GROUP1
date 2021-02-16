d4be440e1224218290936761f26ff78a7fa600b6
thisisminh172 <thisisminh172@gmail.com>
2021-02-16 08:20:35 +0700

    add models and dbcontext

---
+++ source-code/YashSolution/YashSolution.Data/Entities/Certification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YashSolution.Data.Entities
+{
+    public class Certification
+    {
+        public int Id { get; set; }
+        public string CertifyType { get; set; }
+
+
+        public List<Item> Items { get; set; }
+    }
+}
